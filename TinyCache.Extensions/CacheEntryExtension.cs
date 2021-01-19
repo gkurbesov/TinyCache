c13c78817463ec8d436fc1182ba9dded92a1bee7
Gennady <ltrg67@gmail.com>
2021-01-19 21:39:25 +0300

    added extensions

---
+++ TinyCache.Extensions/CacheEntryExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyCache.Extensions
+{
+    public static class CacheEntryExtension
+    {
+        public static ICacheEntry<T> SetAbsoluteExpiration<T>(this ICacheEntry<T> entry, TimeSpan time) where T: class
+        {
+            entry.AbsoluteExpiration = DateTimeOffset.UtcNow.Add(time);
+            return entry;
+        }
+
+        public static ICacheEntry<T> SetSlidingExpiration<T>(this ICacheEntry<T> entry, TimeSpan time) where T : class
+        {
+            entry.SlidingExpiration = time;
+            return entry;
+        }
+
+        public static ICacheEntry<T> SetPriority<T>(this ICacheEntry<T> entry, CacheItemPriority priority) where T : class
+        {
+            entry.Priority = priority;
+            return entry;
+        }
+
+        public static ICacheEntry<T> SetValue<T>(this ICacheEntry<T> entry, T value) where T : class
+        {
+            entry.Value = value;
+            return entry;
+        }
+    }
+}
