bb939d99930faa2c6de27c11edb6eafb96b2f2e8
Gennady <ltrg67@gmail.com>
2021-01-24 23:24:18 +0300

    new unit tests

--- TinyCache.Tests/MemoryCacheTest.cs
+++ TinyCache.Tests/MemoryCacheTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using Xunit;
+using TinyCache.Extensions;
 
 namespace TinyCache.Tests
 {
@@ -111,6 +112,49 @@ namespace TinyCache.Tests
             Assert.True(cache.TryGetValue(1, out var tmp1));
             Thread.Sleep(3500);
             Assert.False(cache.TryGetValue(1, out var tmp2));
+        }
+
+
+        [Fact]
+        public void LimitTest1()
+        {
+            IMemoryCache<object> cache = new MemoryCache<object>(
+                new MemoryCacheOptions()
+                {
+                    EntriesSizeLimit = 3,
+                    ExpirationScanFrequency = TimeSpan.FromSeconds(30)
+                });
+
+            cache.CreateEntry(1).SetPriority(CacheItemPriority.NeverRemove);
+            cache.CreateEntry(2).SetPriority(CacheItemPriority.Low);
+            cache.CreateEntry(3).SetPriority(CacheItemPriority.Normal);
+            cache.CreateEntry(4).SetPriority(CacheItemPriority.Normal);
+
+            Assert.False(cache.ContainsKey(2));
+            Assert.True(cache.ContainsKey(1));
+            Assert.True(cache.ContainsKey(3));
+            Assert.True(cache.ContainsKey(4));
+            Assert.True(cache.TrySet(5, new CacheEntry<object>(5)));
+
+        }
+
+
+        [Fact]
+        public void LimitTest2()
+        {
+            IMemoryCache<object> cache = new MemoryCache<object>(
+                new MemoryCacheOptions()
+                {
+                    EntriesSizeLimit = 3,
+                    ExpirationScanFrequency = TimeSpan.FromSeconds(30)
+                });
+
+            cache.CreateEntry(1).SetPriority(CacheItemPriority.NeverRemove);
+            cache.CreateEntry(2).SetPriority(CacheItemPriority.NeverRemove);
+            cache.CreateEntry(3).SetPriority(CacheItemPriority.NeverRemove);
+
+            Assert.Throws<IndexOutOfRangeException>(() => { cache.CreateEntry(4); });
+            Assert.False(cache.TrySet(4, new CacheEntry<object>(4)));
         }
     }
 }
